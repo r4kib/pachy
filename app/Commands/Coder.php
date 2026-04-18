6a2386721e1cec2da3bf53d50cdc1da08882cbb0
rakib <rakibatwork24@gmail.com>
2026-04-18 23:17:29 +0600

    add agent and mcp

---
+++ app/Commands/Coder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class Coder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:coder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Write Code';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+
+    /**
+     * Define the command's schedule.
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
